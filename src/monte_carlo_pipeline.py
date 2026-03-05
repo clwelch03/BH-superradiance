b526f675465889ed766afa1d03415ad727bc223d
clwelch03 <catherinewelch@proton.me>
2026-03-05 10:03:18 -0800

    feat: very beginnings of new MC pipeline

---
+++ src/monte_carlo_pipeline.py
@@ -0,0 +1,6 @@
+from gwpopulation.models 
+from bilby.core.result import read_in_result
+from scipy.interpolate import interp1d
+import numpy as np
+import deepdish as dd
+import matplotlib.pyplot as plt
